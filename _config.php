0b4e437384b4d7b607bbeb22e2deaf19e12ea09b
Allen McCabe <allenmccabe@gmail.com>
2013-07-25 08:36:32 -0700

    Added _config file with SIDEBARS_DIR and SIDEBARS_PATH constants.

---
+++ _config.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * - SIDEBARS_DIR: Path relative to webroot, e.g. "sidebars"
+ * - SIDEBARS_PATH: Absolute filepath, e.g. "/var/www/my-webroot/sidebars"
+ */
+define('SIDEBARS_DIR', 'sidebars');
+define('SIDEBARS_PATH', BASE_PATH . '/' . SIDEBARS_DIR);
